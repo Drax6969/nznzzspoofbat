51d97070239be67dc2fc9583b9c0e7ef5e7225c2
Drax6969 <119931496+Drax6969@users.noreply.github.com>
2023-07-27 01:02:40 +0200

    Add files via upload

---
+++ zyz354d9.bat
@@ -0,0 +1,25 @@
+cd C:\Windows\SysWOW64\nl-NL
+zyz354d9.exe /SU AUTO
+zyz354d9.exe /IV NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /ID NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /SM NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /SP NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /SV NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /SS NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /SK NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /SF NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /PSN NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /BM NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /BP NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /BV NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /BS NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /BT NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /CM NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /CT NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /CV NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /CS NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /CA NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /CO NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /OS NZNZZ-%random%%random%%random%%random%
+zyz354d9.exe /SCO NZNZZ-%random%%random%%random%%random%
+
